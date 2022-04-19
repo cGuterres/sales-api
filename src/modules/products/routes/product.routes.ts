8fee698ce9e3bfe89447abc469bdebedcf6ac827
Christian Guterres Martins <christian.martins@poatek.com>
2022-04-18 22:43:40 -0300

    Add celebrate to validation requests

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productController = new ProductController();
 
 productsRouter.get('/', productController.getAll);
-productsRouter.get('/:id', productController.getProductDetail);
-productsRouter.post('/', productController.create);
-productsRouter.put('/:id', productController.update);
-productsRouter.delete('/:id', productController.delete);
+
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.getProductDetail,
+);
+
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().max(50).required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().max(50).required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productsRouter;
